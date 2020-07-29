ca2ea0abb604abe17c66b5f5314f8e443bb84ad0
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-07-29 16:47:14 +0100

    Add two buttons

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Mokus!</Text>
+      <Text
+        style={styles.title}
+        >Hello Mokus!</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}>
+            <Text>Croissant</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}>
+            <Text>Coffee</Text>
+          </TouchableOpacity>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +30,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  button: {
+    marginVertical: 10,
+    marginHorizontal: 10,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    width: 100,
+    alignItems: 'center'
+  },
+  title: {
+    marginVertical: 10
+  }
 });
