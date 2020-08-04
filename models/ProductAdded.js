f16b3357144641b16e5400a8ce4095b0d76b8065
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-08-04 17:03:39 +0100

    Add ProductAdded model

---
+++ models/ProductAdded.js
@@ -0,0 +1,10 @@
+class ProductAdded {
+  constructor(quantity, price, name, sum) {
+    this.quantity = quantity;
+    this.price = price;
+    this.name = name;
+    this.sum = sum;
+  }
+}
+
+export default ProductAdded;
